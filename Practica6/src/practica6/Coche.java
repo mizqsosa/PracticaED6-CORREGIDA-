cf27e9b467d20f469e1c37c5ef9b8d461be1c3d3
ManuyBlanca <ManuyBlanca@LAPTOP-V03IKIHM.home>
2019-05-23 23:56:33 +0200

    Implementar los métodos #5

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -2,39 +2,62 @@ package practica6;
 
 public class Coche {
 	String matricula;
-	String atrib;
+	String combustible;
 	String modelo;
 	String fabricante;
 
 	public Coche() {
 		matricula = "";
-		atrib = "";
+		combustible = "";
 		modelo = "";
 		fabricante = "";
 	}
 
 	public Coche(String m, String c, String mo, String f) {
 		matricula = m;
-		atrib = c;
+		combustible = c;
 		modelo = mo;
 		fabricante = f;
 	}
 
-	public String metodo1() {
+	public String AtributosCoche() {
 		String resultado = "";
-		if (atrib == "Gasolina") {
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoA(modelo, fabricante);
-		} else if (atrib == "Diesel") {
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoB(modelo, fabricante);
-		} else if (atrib == "Híbrido") {
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoC(modelo, fabricante);
-		} else {
-			resultado += "El coche con matricula " + matricula + " ";
+		String texto = "El coche con matricula ";
+		switch (combustible) {
+		case "Gasolina": 
+			resultado += texto + matricula + " ";
+			resultado += ConsumoCocheA("Corsa", "Opel");
+			break;
+		case "Diesel":
+			resultado += texto + matricula + " ";
+			resultado += ConsumoCocheB("Ibiza", "Seat");
+			break;
+		case "Híbrido":
+			resultado += texto + matricula + " ";
+			resultado += ConsumoCocheC("Getz", "Hyundai");
+			break;
+		default:
+			resultado += texto + matricula + " ";
 			resultado += "no dispone de información";
+			break;
 		}
+		return resultado;
+	}
+	public String ConsumoCocheA(String modelo, String fabricante) {
+		String resultado = "";
+		resultado = "es un " + fabricante + modelo + " y gasta 1,337 euros por litro";
+		return resultado;	
+	}
+
+	public String ConsumoCocheB(String modelo, String fabricante) {
+		String resultado = "";
+		resultado = "es un " + fabricante + modelo + " y gasta 1,052 euros por litro";
+		return resultado; 
+	}
+
+	public String ConsumoCocheC(String modelo, String fabricante) {
+		String resultado = "";
+		resultado = "es un " + fabricante + modelo + " y no necesita combustible";
 		return resultado;
 	}
 }
