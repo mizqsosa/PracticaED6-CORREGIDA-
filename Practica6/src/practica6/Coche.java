b95d7e078749c2ab8984482cebfb033789e7e1a7
ManuyBlanca <ManuyBlanca@LAPTOP-V03IKIHM.home>
2019-05-24 00:45:56 +0200

    Documentación JavaDoc #6

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -1,60 +1,102 @@
 package practica6;
 
 public class Coche {
+	
+	/**
+	 * Clase Coche
+	 * Contiene información de cada coche (matrícula, combustible, modelo y fabricante)
+	 * Además contiene los métodos AtributosCoche, ConsumoCocheA, ConsumoCocheB y ConsumoCocheC
+	 * @author Manuel Izquierdo Sosa
+	 * @version 1.1
+	 */
+	
+	/**
+	 * Matrícula del coche
+	 */
 	String matricula;
+	/**
+	 * Combustible que usa el coche 
+	*/
 	String combustible;
+	/** 
+	 * Modelo del coche 
+	*/
 	String modelo;
+	/**
+	 * Fabricante del coche
+	 */
 	String fabricante;
 
+	/**
+	 * Constructor
+	 */
 	public Coche() {
+		
 		matricula = "";
 		combustible = "";
 		modelo = "";
 		fabricante = "";
 	}
-
+	/**
+	 * Constructor con 4 parámetros
+	 * @param m //matricula del coche
+	 * @param c //combustible que usa el coche
+	 * @param mo //modelo del coche
+	 * @param f //fabricante del coche
+	 */
 	public Coche(String m, String c, String mo, String f) {
 		matricula = m;
 		combustible = c;
 		modelo = mo;
 		fabricante = f;
 	}
-
+	/**
+	 * Método AtributosCoche
+	 * Describe los atributos de los coches dependiendo del tipo de combustible que utilizan
+	 * @return <ul>
+	 * 			  <li>"Gasolina": "El coche con matricula " + matricula + " " + ConsumoCocheA</li>
+	 * 			  <li>"Diesel": "El coche con matricula " + matricula + " " + ConsumoCocheB</li>
+	 * 			  <li>"Híbrido": "El coche con matricula " + matricula + " " + ConsumoCocheC</li>
+	 * 			  <li> Por defecto: "El coche con matricula " + matricula + " " + "no dispone de información"</li> 
+	 * 		   </ul>
+	 */
 	public String AtributosCoche() {
 		String resultado = "";
 		String texto = "El coche con matricula ";
-		switch (combustible) {
-		case "Gasolina": 
-			resultado += texto + matricula + " ";
-			resultado += ConsumoCocheA("Corsa", "Opel");
-			break;
-		case "Diesel":
-			resultado += texto + matricula + " ";
-			resultado += ConsumoCocheB("Ibiza", "Seat");
-			break;
-		case "Híbrido":
-			resultado += texto + matricula + " ";
-			resultado += ConsumoCocheC("Getz", "Hyundai");
-			break;
-		default:
-			resultado += texto + matricula + " ";
-			resultado += "no dispone de información";
-			break;
-		}
+		
 		return resultado;
 	}
+	/**
+	 * Método ConsumoCocheA
+	 * Define lo que gasta un coche en función de su fabricante y modelo
+	 * @param modelo modelo del coche
+	 * @param fabricante fabricante del coche
+	 * @return <p>"es un " + fabricante + modelo + " y gasta 1,337 euros por litro"</p>
+	 */
 	public String ConsumoCocheA(String modelo, String fabricante) {
 		String resultado = "";
 		resultado = "es un " + fabricante + modelo + " y gasta 1,337 euros por litro";
 		return resultado;	
 	}
-
+	/**
+	 * Método ConsumoCocheB
+	 * Define lo que gasta un coche en función de su fabricante y modelo
+	 * @param modelo modelo del coche
+	 * @param fabricante fabricante del coche
+	 * @return <p>"es un " + fabricante + modelo + " y gasta 1,052 euros por litro"</p>
+	 */
 	public String ConsumoCocheB(String modelo, String fabricante) {
 		String resultado = "";
 		resultado = "es un " + fabricante + modelo + " y gasta 1,052 euros por litro";
 		return resultado; 
 	}
-
+	/**
+	 * Método ConsumoCocheC
+	 * Define lo que gasta un coche en función de su fabricante y modelo
+	 * @param modelo modelo del coche
+	 * @param fabricante fabricante del coche
+	 * @return <p>"es un " + fabricante + modelo + " y no necesita combustible"</p>
+	 */
 	public String ConsumoCocheC(String modelo, String fabricante) {
 		String resultado = "";
 		resultado = "es un " + fabricante + modelo + " y no necesita combustible";
--- Practica6/src/practica6/Moto.java
+++ Practica6/src/practica6/Moto.java
@@ -1,31 +1,58 @@
 package practica6;
 
+/**
+* Clase Moto
+* Contiene información de cada moto (matrícula, cilindrada)
+* Además contiene los getter y setters correspondientes
+* @author Manuel Izquierdo Sosa
+* @version 1.1
+*/
 public class Moto {
+	/**
+	 * matrícula de la moto
+	 */
 	String matricula;
+	/**
+	 * cilindrada de la moto
+	 */
 	int cilindrada;
-
+	/**
+	 * Constructor
+	 */
 	public Moto() {
 		matricula = "";
 		cilindrada = 0;
 	}
-
+	/**
+	 * Constructor con 2 parámetros
+	 * @param m //matrícula de la moto
+	 * @param c //cilindrada de la moto
+	 */
 	public Moto(String m, int c) {
 		matricula = m;
 		cilindrada = c;
 	}
-
+	/**
+	 * @return matricula
+	 */
 	public String getMatricula() {
 		return matricula;
 	}
-
+	/**
+	 * @return cilindrada
+	 */
 	public int getCilindrada() {
 		return cilindrada;
 	}
-
+	/**
+	 * @param m //Cambia matricula
+	 */
 	public void setMatricula(String m) {
 		matricula = m;
 	}
-
+	/**
+	 * @param c //Cambia cilindrada
+	 */
 	public void setCilindrada(int c) {
 		cilindrada = c;
 	}
