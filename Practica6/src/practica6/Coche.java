c345f65aa2a382977623c135c33b5985e80a7a95
ManuyBlanca <ManuyBlanca@LAPTOP-V03IKIHM.home>
2019-05-23 21:55:24 +0200

    Primera Subida #4

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -2,44 +2,38 @@ package practica6;
 
 public class Coche {
 	String matricula;
-	String combustible;
+	String atrib;
 	String modelo;
 	String fabricante;
 
 	public Coche() {
 		matricula = "";
-		combustible = "";
+		atrib = "";
 		modelo = "";
 		fabricante = "";
 	}
 
 	public Coche(String m, String c, String mo, String f) {
 		matricula = m;
-		combustible = c;
+		atrib = c;
 		modelo = mo;
 		fabricante = f;
 	}
 
-	public String AtributoCoche() {
+	public String metodo1() {
 		String resultado = "";
-		String texto = "El coche con matricula ";
-		switch (combustible) {
-		case "Gasolina": 
-			resultado += texto + matricula + " ";
-			resultado += ConsumoCocheA(modelo, fabricante);
-			break;
-		case "Diesel":
-			resultado += texto + matricula + " ";
-			resultado += ConsumoCocheB(modelo, fabricante);
-			break;
-		case "Híbrido":
-			resultado += texto + matricula + " ";
-			resultado += ConsumoCocheC(modelo, fabricante);
-			break;
-		default:
-			resultado += texto + matricula + " ";
+		if (atrib == "Gasolina") {
+			resultado += "El coche con matricula " + matricula + " ";
+			resultado += metodoA(modelo, fabricante);
+		} else if (atrib == "Diesel") {
+			resultado += "El coche con matricula " + matricula + " ";
+			resultado += metodoB(modelo, fabricante);
+		} else if (atrib == "Híbrido") {
+			resultado += "El coche con matricula " + matricula + " ";
+			resultado += metodoC(modelo, fabricante);
+		} else {
+			resultado += "El coche con matricula " + matricula + " ";
 			resultado += "no dispone de información";
-			break;
 		}
 		return resultado;
 	}
